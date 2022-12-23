bb714406a840b5c78d54910de2645949ae6d8f3e
Лопатин Александр <lopatin@CKP.local>
2022-12-23 11:05:08 +0300

    biulded

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 
 ReactDOM.render(
-	<App/>,
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">xD</div>
+		<div className="card">^_^</div>
+		<div className="card">=-'</div>
+	</div>,
 	document.querySelector("#root")
-);
+)
+
+
